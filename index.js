a761096bbf59daabacd4cdb3680f06bc29505e1a
JuhilGajera12 <juhil.gajera@novicbase.com>
2025-07-21 15:25:41 +0530

    updated code

--- index.js
+++ index.js
@@ -8,67 +8,101 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+// app.post("/webhook", async (req, res) => {
+//   console.log("Received webhook request...");
+//   const secret = process.env.ELEVENLABS_CONVAI_WEBHOOK_SECRET;
+//   const { event, error } = await constructWebhookEvent(req, secret);
+
+//   if (error) {
+//     return res.json({ error: error }, { status: 401 });
+//   }
+//   if (event.type === "post_call_transcription") {
+//     console.log("event data", JSON.stringify(event.data, null, 2));
+//   }
+
+//   return res.json({
+//     success: true,
+//     message: "Webhook received successfully",
+//   });
+// });
+
+// const constructWebhookEvent = async (req, secret) => {
+//   console.log("🚀 ~ constructWebhookEvent ~ req:", req);
+//   const body = await req;
+//   const signature_header = req.headers.get("ElevenLabs-Signature");
+//   console.log(signature_header);
+
+//   if (!signature_header) {
+//     return { event: null, error: "Missing signature header" };
+//   }
+
+//   const headers = signature_header.split(",");
+//   const timestamp = headers.find((e) => e.startsWith("t="))?.substring(2);
+//   const signature = headers.find((e) => e.startsWith("v0="));
+
+//   if (!timestamp || !signature) {
+//     return { event: null, error: "Invalid signature format" };
+//   }
+
+//   // Validate timestamp
+//   const reqTimestamp = Number(timestamp) * 1000;
+//   const tolerance = Date.now() - 30 * 60 * 1000;
+//   if (reqTimestamp < tolerance) {
+//     return { event: null, error: "Request expired" };
+//   }
+
+//   // Validate hash
+//   const message = `${timestamp}.${body}`;
+
+//   if (!secret) {
+//     return { event: null, error: "Webhook secret not configured" };
+//   }
+
+//   const digest =
+//     "v0=" + crypto.createHmac("sha256", secret).update(message).digest("hex");
+//   console.log({ digest, signature });
+
+//   if (signature !== digest) {
+//     return { event: null, error: "Invalid signature" };
+//   }
+
+//   const event = JSON.parse(body);
+//   return { event, error: null };
+// };
+
 app.post("/webhook", async (req, res) => {
-  console.log("Received webhook request...");
   const secret = process.env.ELEVENLABS_CONVAI_WEBHOOK_SECRET;
-  const { event, error } = await constructWebhookEvent(req, secret);
-
-  if (error) {
-    return res.json({ error: error }, { status: 401 });
-  }
-  if (event.type === "post_call_transcription") {
-    console.log("event data", JSON.stringify(event.data, null, 2));
-  }
-
-  return res.json({
-    success: true,
-    message: "Webhook received successfully",
-  });
-});
-
-const constructWebhookEvent = async (req, secret) => {
-  console.log("🚀 ~ constructWebhookEvent ~ req:", req);
-  const body = await req.text();
-  const signature_header = req.headers.get("ElevenLabs-Signature");
-  console.log(signature_header);
-
-  if (!signature_header) {
-    return { event: null, error: "Missing signature header" };
-  }
-
-  const headers = signature_header.split(",");
-  const timestamp = headers.find((e) => e.startsWith("t="))?.substring(2);
+  const headers = req.headers["ElevenLabs-Signature"].split(",");
+  const timestamp = headers.find((e) => e.startsWith("t=")).substring(2);
   const signature = headers.find((e) => e.startsWith("v0="));
-
-  if (!timestamp || !signature) {
-    return { event: null, error: "Invalid signature format" };
-  }
-
   // Validate timestamp
-  const reqTimestamp = Number(timestamp) * 1000;
+  const reqTimestamp = timestamp * 1000;
   const tolerance = Date.now() - 30 * 60 * 1000;
   if (reqTimestamp < tolerance) {
-    return { event: null, error: "Request expired" };
+    res.status(403).send("Request expired");
+    return;
+  } else {
+    // Validate hash
+    const message = `${timestamp}.${req.body}`;
+    const digest =
+      "v0=" + crypto.createHmac("sha256", secret).update(message).digest("hex");
+    if (signature !== digest) {
+      res.status(401).send("Request unauthorized");
+      return;
+    }
   }
 
-  // Validate hash
-  const message = `${timestamp}.${body}`;
+  let body = "";
+  // Handle chunked/streaming requests
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
 
-  if (!secret) {
-    return { event: null, error: "Webhook secret not configured" };
-  }
-
-  const digest =
-    "v0=" + crypto.createHmac("sha256", secret).update(message).digest("hex");
-  console.log({ digest, signature });
-
-  if (signature !== digest) {
-    return { event: null, error: "Invalid signature" };
-  }
-
-  const event = JSON.parse(body);
-  return { event, error: null };
-};
+  req.on("end", () => {
+    const requestBody = JSON.parse(body);
+    console.log("Received webhook request:", requestBody);
+  });
+});
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
